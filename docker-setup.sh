c1712801ee0dbdb8d0d92e75fbd6e5036aaa2d53
kendofriendo <39413174+kendofriendo@users.noreply.github.com>
2023-11-30 16:15:00 -0500

    Update docker-setup.sh
    replaced main repo with fork repo url

--- docker-setup.sh
+++ docker-setup.sh
@@ -16,8 +16,8 @@ install_dir=${install_dir:-/usr/share/microbin}
 # Create directory and download files
 mkdir -p $install_dir
 cd $install_dir
-$download_command https://raw.githubusercontent.com/szabodanika/microbin/master/.env
-$download_command https://raw.githubusercontent.com/szabodanika/microbin/master/compose.yaml
+$download_command https://raw.githubusercontent.com/kendofriendo/microbin/master/.env
+$download_command https://raw.githubusercontent.com/kendofriendo/microbin/master/compose.yaml
 
 # Get public path URL and port from user
 echo -e "\033[1mEnter public path URL (e.g. https://microbin.myserver.net or http://localhost:8080):\033[0m"
